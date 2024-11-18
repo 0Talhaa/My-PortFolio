166ee41bc22071b367a8ba491421021c63a77319
0Talhaa <mirzakhan4581207talha@gmail.com>
2024-11-18 05:50:06 +0500

    This is First commit in angular project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CarouselComponent } from './carousel/carousel.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NavbarComponent,CarouselComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
